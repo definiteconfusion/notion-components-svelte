89eca6ca87acf5fe7b05810e60e60f1af7c87f81
definiteconfusion <122401194+definiteconfusion@users.noreply.github.com>
2024-10-29 12:53:12 -0400

    'Init commit'

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Button from './lib/Buttons/Button.svelte';
+  let count = $state(0);
+  const increment = () => count += 1;
+</script>
+
+<Button mode="light" onClick={() => increment()}>Count: {count}</Button>
---
+++ src/lib/Buttons/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { name, mode, onClick, className, style } = $props();
+</script>
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/definiteconfusion/notion-components/src/notion-components/Buttons/Buttons.css">
+</svelte:head>
+
+<button class={`button-main ${mode} ${className}`} onclick={onClick} style={style} >
+    <slot></slot>
+</button>
