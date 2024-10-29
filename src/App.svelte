6c874b940f46c22bbcbf4f784f6e852bcc63b645
definiteconfusion <122401194+definiteconfusion@users.noreply.github.com>
2024-10-29 13:03:22 -0400

    'added `WideButton` component'

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Button from './lib/Buttons/Button.svelte';
+  import WideButton from './lib/Buttons/WideButton.svelte';
   let count = $state(0);
   const increment = () => count += 1;
 </script>
 
 <Button mode="light" onClick={() => increment()}>Count: {count}</Button>
+<div style="width: 10rem;"><WideButton mode="light" onClick={() => increment()}>Count: {count}</WideButton></div>
---
+++ src/lib/Buttons/WideButton.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { name, mode, onClick, className, style, edging } = $props();
+</script>
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/definiteconfusion/notion-components/src/notion-components/Buttons/Buttons.css">
+</svelte:head>
+
+<button class={`button-main max-button-width ${mode} ${className}`} onclick={onClick} style={`margin-inline:${edging};${style}`} >
+    <slot></slot>
+</button>
